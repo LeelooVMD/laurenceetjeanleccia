4e17c24b06630470b6d842f654f2abe7f68efcd2
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-09 15:53:54 +0100

    card qrticle

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Destroying articles'
+Article.destroy_all
+
+puts 'Adding articles'
+Article.create(title: 'Exposition Laurence LECCIA & Jean LECCIA à Montpellier les 2,3,9 et 10 juin 2018', picture: 'expo-montpellier.jpeg', content: 'Laurence LECCIA & Jean LECCIA exposent à Montpellier les 2,3,9 et 10 juin 2018 de 11h à 13h & de 15h à 20h.
+8 Rue Foch – Entrée libre', user_id: 1)
