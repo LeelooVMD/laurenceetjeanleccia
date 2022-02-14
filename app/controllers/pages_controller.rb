560a12c07408c4831e7692fb621a6d21b8358009
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-14 15:42:56 +0100

    static laurenceleccia page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :laurenceleccia ]
 
   def home
+  end
+
+  def laurenceleccia
   end
 end
