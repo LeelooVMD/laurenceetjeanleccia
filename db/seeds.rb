ce7307519752d622a057c45d73702de383e8c082
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-14 11:19:01 +0100

    seeds + cards articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ Article.destroy_all
 puts 'Adding articles'
 Article.create(title: 'Exposition Laurence LECCIA & Jean LECCIA', picture: 'expo-montpellier.jpeg', content: 'Laurence LECCIA & Jean LECCIA exposent à Montpellier les 2,3,9 et 10 juin 2018 de 11h à 13h & de 15h à 20h.
 8 Rue Foch – Entrée libre', user_id: 1)
+Article.create(title: 'Exposition Laurence LECCIA à Sète', picture: 'sete-invitation.png', content: 'Exposition du 16/10/2015 au 25/10/2015 au 16 Rue Gabriel Péri.', user_id: 1)
+Article.create(title: 'Exposition "Entre les chalets" à Gruissan', picture: 'gruissan-exposition.jpeg', content: 'Exposition de Laurence Leccia en 2016 à Gruissan Plage.', user_id: 1)
+Article.create(title: 'Exposition Laurence LECCIA à Montpellier', picture: 'montpellier-exposition.jpeg', content: 'Exposition en 1995 à Les Passagers de l\'art au 3 Rue Nozeran.', user_id: 1)
