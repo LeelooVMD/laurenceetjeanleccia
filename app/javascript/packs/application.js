526ded42db601ed432f2f0c5e629c61505d10039
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-16 17:02:42 +0100

    controller stimulus pour carrousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("about_tables")
-require("carrouel")
+// require("carrousel")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
