6fd6959f9f424ff6d28aa7d0eaedb4cac0c455d4
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-16 15:03:13 +0100

    js fixed for tables

--- app/javascript/jscard.js
+++ app/javascript/jscard.js
@@ -1,4 +1,4 @@
-// tabs
+//------ Tableau 1 avec textes -------
 
 const tabLinks = document.querySelectorAll(".tablinks");
 const tabContent = document.querySelectorAll(".tabcontent");
@@ -19,6 +19,31 @@ function openTabs(el) {
   });
 
   tabLinks.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  document.getElementById(name).classList.add("active");
+  btnTarget.classList.add("active");
+}
+
+//------ Tableau 2 avec biographie -------
+
+const tabLinks2 = document.querySelectorAll(".tablinks2");
+const tabContent2 = document.querySelectorAll(".tabcontent2");
+
+tabLinks2.forEach(function (el) {
+  el.addEventListener("click", openTabs2);
+});
+
+function openTabs2(el) {
+  const btnTarget = el.currentTarget;
+  const name = btnTarget.dataset.name;
+
+  tabContent2.forEach(function (el) {
+    el.classList.remove("active");
+  });
+
+  tabLinks2.forEach(function (el) {
     el.classList.remove("active");
   });
 
