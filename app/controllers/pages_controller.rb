e41885fcdbb1df620e808e8ed525d4bbc6a5eff7
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-02-16 16:16:21 +0100

    routes and testing carroussels

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :laurenceleccia ]
+  skip_before_action :authenticate_user!, only: [ :home, :laurenceleccia, :laurence_lestoiles ]
 
   def home
   end
@@ -7,4 +7,8 @@ class PagesController < ApplicationController
   def laurenceleccia
     @texts = Text.all
   end
+
+  def laurence_lestoiles
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/laurenceleccia', to: "pages#laurenceleccia"
+  get 'pages/laurenceleccia/lestoiles', to: "pages#laurence_lestoiles"
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
