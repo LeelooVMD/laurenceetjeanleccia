5caf8c85af8f56e5f83b46d5d691327390afb2fd
Lilas Marchand <lilasviolettemarchand@gmail.com>
2022-05-31 14:43:34 +0200

    seeds

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'laurenceleccia/lestoiles', to: "inbetweens#index"
   get 'jeanleccia/lestoiles', to: "pages#jean_lestoiles"
   resources :articles
+  resources :pictures
+  resources :collections
   resources :inbetweens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
